1966fed2326de5bcdb28bdd346348bef7fc79853
tkabeee <tkabee@gmail.com>
2017-10-24 23:38:55 +0900

    各イベントハンドリングを用意

--- src/main.js
+++ src/main.js
@@ -7,9 +7,43 @@ import router from './router'
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
-new Vue({
+var app = new Vue({
   el: '#app',
   router,
   template: '<App/>',
-  components: { App }
+  components: { App },
+  data: {
+    now: '',
+    hour: '',
+    minute: ''
+  },
+
+  watch: {
+
+  },
+
+  beforeCreate: function () {
+  },
+
+  created: function () {
+  },
+
+  beforeMount: function () {
+  },
+
+  mounted: function () {
+    console.log(app)
+  },
+
+  beforeUpdate: function () {
+  },
+
+  updated: function () {
+  },
+
+  beforeDestroy: function () {
+  },
+
+  destroyed: function () {
+  }
 })
