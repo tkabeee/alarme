8d8b80214dd7b5d1fded4467f1f4f65b558aba74
tkabeee <tkabee@gmail.com>
2017-11-10 09:43:06 +0900

    分を更新するアクション追加

--- src/store/actions.js
+++ src/store/actions.js
@@ -3,3 +3,7 @@ import * as types from './mutation-types'
 export const updateHour = ({ commit }) => {
   commit(types.CHANGE_HOUR)
 }
+
+export const updateMinute = ({ commit }) => {
+  commit(types.CHANGE_MINUTE)
+}
